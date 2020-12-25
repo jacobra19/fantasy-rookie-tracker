276abe9c979b4df954e021b592072bc6f53bc481
Yakov Rakhamimov <jacobra19@gmail.com>
2020-12-25 15:42:39 +0200

    refactored flow and naming

--- index.js
+++ index.js
@@ -1,61 +1,33 @@
 require('dotenv').config()
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+const { executePuppeteer } = require('./executePuppeteer')
+const { config } = require('./firebaseConfig')
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+firebase.initializeApp(config);
 
-const puppeteer = require('puppeteer');
-const leagueId = process.env.LEAGUE_ID || ''
-const rostersUrl = `https://fantasy.espn.com/basketball/league/rosters?leagueId=${leagueId}&seasonId=2020`;
-
-
-
-const doPuppeteer = async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto(rostersUrl, {
-        waitUntil: 'networkidle0',
-    });
-
-    await page.click('#onetrust-accept-btn-handler');
-
-    await page.waitForNavigation({
-        waitUntil: 'networkidle0',
-    });
+const rookies = require('./rookies').rookies2020Draft
 
 
-    // await page.screenshot({ path: 'example.png' });
 
-    const stories = await page.evaluate(() => {
-        let teamElement = Array.prototype.slice.call(document.getElementsByClassName('ResponsiveTable'))
-        let rookies = {
-            'Zion Williamson': true,
-            'Ja Morant': true,
-        }
 
-        console.log('teamElement', teamElement)
-        return teamElement.map((teamEl) => {
-            let rosterTable = teamEl.querySelector('.Table__TBODY')
-            let rosterList = Array.prototype.slice.call(rosterTable.getElementsByClassName('player-column__athlete'))
-            console.log('rosterTable', rosterTable)
-            console.log('rosterList', rosterList)
-            let roster = rosterList.map(tr => {
-                console.log(tr)
-                return tr.title
-            })
-            return {
-                team: teamEl.querySelector('.teamName').title,
-                roster,
-                rooks: roster.filter(player => {
-                    return rookies[player]
-                })
+console.log('config', config)
 
-            }
-        })
 
-    });
 
-    console.log(stories)
-    // await page.tracing.stop()
-    await browser.close()
+executePuppeteer(rookies)
+    .then((currentRecord) => {
+        console.log('currentRecord', currentRecord)
+        // // const savedDoc = await db.collection('manual-scrape-dates').add({
+        // //     date: ,
+        // //     teams: currentRecord
+        // // });
 
-}
+        // console.log('savedDoc', savedDoc)
 
-doPuppeteer()
+    }).catch((err) => {
+        console.log('err', err)
+    })
